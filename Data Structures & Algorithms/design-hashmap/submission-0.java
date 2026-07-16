d86a75865bb8b238c67f85bf92b21bce10018aaa
Pranav Verma <pranavverma889@gmail.com>
2026-07-16 18:20:06 +0530

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.java
@@ -0,0 +1,65 @@
+class MyHashMap {
+
+    class Pair {
+        int key;
+        int value;
+
+        Pair(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private static final int SIZE = 1000;
+    private LinkedList<Pair>[] buckets;
+
+    public MyHashMap() {
+        buckets = new LinkedList[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    private int hash(int key) {
+        return key % SIZE;
+    }
+
+    public void put(int key, int value) {
+        int index = hash(key);
+
+        for (Pair pair : buckets[index]) {
+            if (pair.key == key) {
+                pair.value = value;
+                return;
+            }
+        }
+
+        buckets[index].add(new Pair(key, value));
+    }
+
+    public int get(int key) {
+        int index = hash(key);
+
+        for (Pair pair : buckets[index]) {
+            if (pair.key == key) {
+                return pair.value;
+            }
+        }
+
+        return -1;
+    }
+
+    public void remove(int key) {
+        int index = hash(key);
+
+        buckets[index].removeIf(pair -> pair.key == key);
+    }
+}
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
