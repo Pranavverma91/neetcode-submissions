9c69fe2982091ed9a63a841c1eb41eb8776074da
Pranav Verma <pranavverma889@gmail.com>
2026-09-23 15:44:01 +0530

    Add: find-common-characters - submission-0

---
+++ Data Structures & Algorithms/find-common-characters/submission-0.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public List<String> commonChars(String[] words) {
+        int[] minFreq = new int[26];
+        
+        for (char c : words[0].toCharArray()) {
+            minFreq[c - 'a']++;
+        }
+        
+        for (int i = 1; i < words.length; i++) {
+            int[] charCount = new int[26];
+            for (char c : words[i].toCharArray()) {
+                charCount[c - 'a']++;
+            }
+            
+            for (int j = 0; j < 26; j++) {
+                minFreq[j] = Math.min(minFreq[j], charCount[j]);
+            }
+        }
+        
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            while (minFreq[i] > 0) {
+                result.add(String.valueOf((char) ('a' + i)));
+                minFreq[i]--;
+            }
+        }
+        
+        return result;
+    }
+}
